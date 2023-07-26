350a400b6a5be814981ab9270bc001da03a274db
Francisco <francrema00@gmail.com>
2023-07-26 17:46:28 -0300

    titulo

--- TripSage/src/Components/NavBar/NavBar.tsx
+++ TripSage/src/Components/NavBar/NavBar.tsx
@@ -4,6 +4,16 @@ function NavBar() {
   return (
     <>
       <h1>Hola</h1>
+      <header>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
