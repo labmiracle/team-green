663256b4539b4fdb2af8d753c1a85d1296f18ba4
Francisco <francrema00@gmail.com>
2023-08-16 17:00:45 -0300

    Components Fly y Hotel WiP

--- TripSage/src/Components/Fly/Fly.tsx
+++ TripSage/src/Components/Fly/Fly.tsx
@@ -1,5 +1,14 @@
+import "./fly.scss";
+
 function Fly() {
-  return <div>Fly</div>;
+  return (
+    <div id="container-fly">
+      <img
+        src="../../../public/img/background/Flight.png"
+        className="img-fly"
+      />
+    </div>
+  );
 }
 
 export default Fly;
--- TripSage/src/Components/Hotel/Hotel.tsx
+++ TripSage/src/Components/Hotel/Hotel.tsx
@@ -1,5 +1,14 @@
+import "./hotel.scss";
+
 function Hotel() {
-  return <div>Hotel</div>;
+  return (
+    <div id="container-hotel">
+      <img
+        src="../../../public/img/background/Hotel.png"
+        className="img-hotel"
+      />
+    </div>
+  );
 }
 
 export default Hotel;
