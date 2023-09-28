eac93aac0619128c0d20e1fe21bf9819848d728d
Francisco <francrema00@gmail.com>
2023-09-28 18:07:47 -0300

    testing the component

--- TripSage/src/Components/Search/SearchFly.tsx
+++ TripSage/src/Components/Search/SearchFly.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./SearchFly.scss";
+import "./loader.scss";
 
 interface Product {
   id: number;
@@ -9,41 +10,70 @@ interface Product {
 
 function ProductList() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [showLoader, setShowLoader] = useState(true);
 
   useEffect(() => {
-    //solicitud al backend para obtener los productos
-    fetch("/api/products")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        setProducts(data);
-      });
+    const timeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    // Simulación de una solicitud al backend para obtener los productos
+    // Aquí puedes reemplazar esta simulación con tu lógica real.
+    setTimeout(() => {
+      //solicitud al backend para obtener los productos
+      // fetch("/api/products")
+      //   .then(function (response) {
+      //     return response.json();
+      //   })
+      //   .then(function (data) {
+      //     setProducts(data);
+      //   });
+
+      const fakeProducts = [
+        { id: 1, name: "Producto 1", price: 10 },
+        { id: 2, name: "Producto 2", price: 20 },
+        // ... Agrega más productos aquí
+      ];
+      setProducts(fakeProducts);
+    }, 500); // Simulación de tiempo de carga
   }, []);
 
   return (
-    <div className="product-list">
-      <h2>
-        Lista de Productos <hr />
-      </h2>
+    <>
+      {showLoader && <span className="loader"></span>}
+      {!showLoader && (
+        <div className="product-list">
+          <div>
+            <h2>
+              Lista de Productos <hr />
+            </h2>
 
-      <div className="list__container">
-        <ul className="list__ul">
-          {/* {products.map(function (product) {
-          return (
-            <li key={product.id}>
-              {product.name} - ${product.price}
-            </li>
-          );
-        })} */}
+            <div className="list__container">
+              <ul className="list__ul">
+                <li className="list__li">Destino</li>
+                <li className="list__li">Fecha de salida</li>
+                <li className="list__li">Fecha de llegada</li>
+                <li className="list__li">Precio</li>
+              </ul>
 
-          <li className="list__li">Destino</li>
-          <li className="list__li">Fecha de salida</li>
-          <li className="list__li">Fecha de llegada</li>
-          <li className="list__li">Precio</li>
-        </ul>
-      </div>
-    </div>
+              <div className="list__product">
+                {products.map(function (product) {
+                  return (
+                    <span className="product__span" key={product.id}>
+                      {product.name} - ${product.price}
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
